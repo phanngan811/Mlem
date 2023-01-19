98a6716b89c0aafec544fdd924830fcc73dd7906
Man Pham <manpham@everfit.io>
2023-01-19 17:53:54 +0700

    implement bottom nav

--- app/src/main/java/com/example/mlem/SettingsFragment.java
+++ app/src/main/java/com/example/mlem/SettingsFragment.java
@@ -1,64 +1,128 @@
 package com.example.mlem;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.mlem.Model.Tag;
+import com.example.mlem.ViewModel.SettingVM;
+import com.example.mlem.databinding.FragmentSettingsBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    SettingVM settingVM;
+    private View mView;
+    private FragmentSettingsBinding mBinding;
+    private FirebaseUser user;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        mBinding = FragmentSettingsBinding.inflate(inflater, container, false);
+        mView = mBinding.getRoot();
+
+        settingVM = new ViewModelProvider(this).get(SettingVM.class);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        initObservers();
+
+        return mView;
+    }
+
+
+    private void initObservers() {
+
+        Button saveBtn = (Button) mView.findViewById(R.id.btnSave);
+        EditText editTextTag = (EditText) mView.findViewById(R.id.editTag);
+        Button btnLogout = (Button) mView.findViewById(R.id.btnLogout);
+        EditText editTextName = (EditText) mView.findViewById(R.id.edit_text_name);
+
+
+        String displayName = user.getDisplayName();
+        if(displayName.equals("")) {
+            Log.d("-----------USER NAME NOT EXIST-----------", user.getDisplayName());
+        }else {
+            Log.d("-----------USER NAME EXIST-----------", user.getDisplayName());
+            editTextName.setText(user.getDisplayName());
+        }
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                settingVM.insertTag(new Tag("0", editTextTag.getText().toString()));
+                updateName();
+                Toast.makeText(mView.getContext(),"Successfully add Preferences",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                settingVM.logout();
+                goToDashboard();
+            }
+        });
+    }
+
+    private void updateName() {
+        EditText nameEditText = mView.findViewById(R.id.edit_text_name);
+        String updateName = nameEditText.getText().toString();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(updateName)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                        }
+                    }
+                });
+
+    }
+
+    private void goToDashboard() {
+        Intent intent = new Intent(mView.getContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
     }
 }
