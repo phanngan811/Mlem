b5e228be3d8e76bf561545d0a3c4d0aa8dda64e4
Man Pham <manpham@everfit.io>
2023-01-19 15:26:42 +0700

    added dashboard

--- app/src/main/java/com/example/mlem/DashboardFragment.java
+++ app/src/main/java/com/example/mlem/DashboardFragment.java
@@ -1,64 +1,152 @@
 package com.example.mlem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.mlem.Adapter.DashboardBlogRVAdapter;
+import com.example.mlem.Adapter.DashboardIngredientRVAdapter;
+import com.example.mlem.Adapter.DashboardRecipeRVAdapter;
+import com.example.mlem.Model.Blog;
+import com.example.mlem.Model.Ingredient;
+import com.example.mlem.Model.Recipe;
+import com.example.mlem.ViewModel.DashboardVM;
+import com.example.mlem.databinding.FragmentDashboardBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link DashboardFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements InterfaceGroup {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView ingredientsRv;
+    private RecyclerView blogsRv;
+    private RecyclerView recipeRv;
+    private DashboardVM mViewModel;
+    DashboardIngredientRVAdapter ingredientAdapter;
+    DashboardBlogRVAdapter blogAdapter;
+    DashboardRecipeRVAdapter recipeAdapter;
+    private View mView;
+    private FragmentDashboardBinding mBinding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public DashboardFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mViewModel = new ViewModelProvider(this).get(DashboardVM.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mBinding = FragmentDashboardBinding.inflate(inflater, container, false);
+        mView = mBinding.getRoot();
+
+
+        ingredientsRv = mView.findViewById(R.id.idIngredientRV);
+        blogsRv = mView.findViewById(R.id.idBlogRV);
+        recipeRv = mView.findViewById(R.id.idRecipeRV);
+
+        initAdapters();
+        initObservers();
+        return mView;
+    }
+
+
+    private void initObservers() {
+        mViewModel.getIngredients();
+        mViewModel.getBlogs();
+        mViewModel.getRecipes();
+
+
+        mViewModel.getIngredientResults().observe(getActivity(), new Observer<List<Ingredient>>() {
+            @Override
+            public void onChanged(List<Ingredient> ingredients) {
+                ingredientAdapter.setIngredientDataArrayList((ArrayList<Ingredient>) ingredients);
+            }
+        });
+        mViewModel.getBlogResults().observe(getActivity(), new Observer<List<Blog>>() {
+            @Override
+            public void onChanged(List<Blog> blogs) {
+                blogAdapter.setBlogDataArrayList((ArrayList<Blog>) blogs);
+            }
+        });
+        mViewModel.getRecipeResults().observe(getActivity(), new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(List<Recipe> recipes) {
+                recipeAdapter.setRecipeDataArrayList((ArrayList<Recipe>) recipes);
+            }
+        });
+    }
+
+    private void initAdapters() {
+
+        ingredientAdapter = new DashboardIngredientRVAdapter(getActivity(), this);
+        blogAdapter = new DashboardBlogRVAdapter(getActivity(), this);
+        recipeAdapter = new DashboardRecipeRVAdapter(getActivity(), this);
+
+        // setting grid layout manager to implement grid view.
+        // in this method '2' represents number of columns to be displayed in grid view.
+        GridLayoutManager ingredientListLayoutManager=new GridLayoutManager(getActivity(),2);
+
+        // at last set adapter to recycler view.
+        ingredientsRv.setLayoutManager(ingredientListLayoutManager);
+        ingredientsRv.setAdapter(ingredientAdapter);
+
+        LinearLayoutManager blogListLayoutManager
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+
+        blogsRv.setLayoutManager(blogListLayoutManager);
+        blogsRv.setAdapter(blogAdapter);
+
+        LinearLayoutManager recipeListLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+
+        recipeRv.setLayoutManager(recipeListLayoutManager);
+        recipeRv.setAdapter(recipeAdapter);
+    }
+
+    @Override
+    public void onClickBlog(int position) {
+        Intent i = new Intent(mView.getContext(), BlogDetail.class);
+        i.putExtra("blogId", mViewModel.getBlogResults().getValue().get(position).getId());
+        startActivity(i);
+    }
+
+    @Override
+    public void onClickIngredient(int position) {
+        Intent i = new Intent(mView.getContext(), IngredientDetail.class);
+        i.putExtra("ingredientId", mViewModel.getIngredientResults().getValue().get(position).getId());
+        startActivity(i);
+    }
+
+    @Override
+    public void onClickRecipe(int position) {
+        Intent i = new Intent(mView.getContext(), RecipeDetail.class);
+        i.putExtra("recipeId", mViewModel.getRecipeResults().getValue().get(position).getId());
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/mlem/SignIn.java
+++ app/src/main/java/com/example/mlem/SignIn.java
@@ -51,7 +51,7 @@ public class SignIn extends AppCompatActivity {
     }
 
     private void goToDashboard() {
-        Intent intent = new Intent(SignIn.this, DashBoard.class);
+        Intent intent = new Intent(SignIn.this, HomeActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
