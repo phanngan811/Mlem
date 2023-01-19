39354e0c5f285ac5a90965c7f0291335074cec53
Man Pham <manpham@everfit.io>
2023-01-19 15:38:03 +0700

    import missing package

--- app/src/main/java/com/example/mlem/Repository/BlogRepository.java
+++ app/src/main/java/com/example/mlem/Repository/BlogRepository.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.example.mlem.Model.Blog;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
